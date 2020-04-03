0c905e37dc5759c8f6ab0c4f0ab05ba801d4fbd9
h42m0ny <harmonyleclaire@gmail.com>
2020-04-03 16:33:56 +0200

    add ghost content for cave page

---
+++ src/components/sections/ContentBrewerie.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Section, Container } from '@components/global';
+
+const ContentBrewerie = ({data}) => (
+  
+      <Section content>
+        <Container>
+         <img src={data.feature_image} alt={data.slug + " image"}/>
+        <p>
+            {data.plaintext}
+        </p>
+
+
+                 
+        </Container>
+      </Section>
+);
+
+
+export default ContentBrewerie;
