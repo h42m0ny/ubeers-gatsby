ab49077e1cf0c8bef32b384b962b7517390b1390
Tony Cois <cois.tony@gmail.com>
2020-04-19 15:30:05 +0200

    gatsby build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
-const GHOST_URI = process.env.GHOST_URI || "http://localhost:2369"
-const GHOST_TOKEN = process.env.GHOST_TOKEN || "2842c8bf95bfce733b0777f981"
-const STRAPI_URI = process.env.STRAPI_URI || "http://localhost:1337"
+const GHOST_URI =
+  process.env.GHOST_URI ||
+  process.env.secrets.GHOST_URI ||
+  "http://localhost:2369"
+const GHOST_TOKEN =
+  process.env.GHOST_TOKEN ||
+  process.env.secrets.GHOST_TOKEN ||
+  "2842c8bf95bfce733b0777f981"
+const STRAPI_URI =
+  process.env.STRAPI_URI ||
+  process.env.secrets.STRAPI_URI ||
+  "http://localhost:1337"
 
 const path = require("path")
 
