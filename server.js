e702f097aa365cd221e8e6bfc9a269e1eaaf0fff
h42m0ny <harmonyleclaire@gmail.com>
2020-04-23 08:58:26 +0200

    prepare for deployment on clever

--- server.js
+++ server.js
@@ -1,43 +1,17 @@
-const express = require("express")
-const gatsbyExpress = require("gatsby-plugin-express")
-const app = express()
-//const { spawn } = require("child_process")
-//const CLEVER_TOKEN = process.env.CLEVER_TOKEN || ""
-//const CLEVER_SECRET = process.env.CLEVER_SECRET || ""
-/*
-app.get("/webhook/ghost", function(req, res) {
-  var child = spawn(
-    "./node_modules/clever-tools/bin/clever.js login --token " +
-      CLEVER_TOKEN +
-      " --secret " +
-      CLEVER_SECRET +
-      " && ./node_modules/clever-tools/bin/clever.js link app_a23540e1-a005-409a-9c57-d0feb6fcc5b4 --alias ubeers-gatsby && ./node_modules/clever-tools/bin/clever.js restart",
-    {
-      shell: true,
-    }
-  )
-  child.stderr.on("data", function(data) {
-    console.error("", data.toString())
-  })
-  child.stdout.on("data", function(data) {
-    console.log("", data.toString())
-  })
-  child.on("exit", function(exitCode) {
-    console.log("Child exited with code: " + exitCode)
-  })
+const gatsbyExpress = require('gatsby-plugin-express');
+const app = express();
 
-  res.send("GET request to the homepage")
-})
-*/
 // serve static files before gatsbyExpress
+app.use(express.static('public/'));
 app.use(express.static("public/"))
 app.use(
-  gatsbyExpress("config/gatsby-express.json", {
-    publicDir: "public/",
-    redirectSlashes: true,
-  })
+    gatsbyExpress("config/gatsby-express.json", {
+        publicDir: "public/",
+        redirectSlashes: true,
+    })
 )
 
-app.listen(8080, function() {
-  console.log("App started on port 8080")
-})
+app.listen(process.env.PORT || '8080', function () {
+    console.log(`App started on port 3000 ${process.env.port}`);
+});
+
